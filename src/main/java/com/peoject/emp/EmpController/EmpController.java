1b9ee1633508ba7c363aa1c21a2b26b66f11d32b
harshpatel-22 <harshvpatel1508@gmail.com>
2024-07-03 12:35:07 +0530

    added cross origin

--- src/main/java/com/peoject/emp/EmpController/EmpController.java
+++ src/main/java/com/peoject/emp/EmpController/EmpController.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.peoject.emp.EmpService.EmpService;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 public class EmpController {
 
     @Autowired
